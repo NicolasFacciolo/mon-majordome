ea4c8d89d9727585fadf399d214318a8edfb2a31
Nico <facciolo.nicolas@gmail.com>
2015-07-10 17:01:06 +0200

    project creation

---
+++ config/settings.inc.php
@@ -0,0 +1,14 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'prestashop');
+define('_DB_USER_', 'monmajordome');
+define('_DB_PASSWD_', 'monmajordome');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'Ug8uZcEjLrhY2OrxdhfdkhmUGRK05bmsYGDEyU2TonMR1KUJxApioMqm');
+define('_COOKIE_IV_', 'oHPBFBMI');
+define('_PS_CREATION_DATE_', '2015-07-10');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.0');
